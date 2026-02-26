0ed6c981cc755557ccf822a5248a149e03396093
Jucott <jcjcnn06@gmail.com>
2026-02-26 18:33:51 +0100

    finalisation Entreprises

--- app/views/entreprise/show.php
+++
@@ -1,7 +0,0 @@
-<h2><?= htmlspecialchars($entreprise['nom']) ?></h2>
-
-<p><?= htmlspecialchars($entreprise['description']) ?></p>
-<p>Email : <?= htmlspecialchars($entreprise['email']) ?></p>
-<p>Téléphone : <?= htmlspecialchars($entreprise['telephone']) ?></p>
-
-<a href="/entreprise/recherche">Retour</a>
